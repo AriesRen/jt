ff7a66059336991cc96de7860bd36e9d40955ad3
renhj <18435175817@163.com>
2018-11-23 18:48:15 +0800

    Add role

--- jt-manager/src/main/java/org/renhj/service/impl/SysUserServiceImpl.java
+++ jt-manager/src/main/java/org/renhj/service/impl/SysUserServiceImpl.java
@@ -1,44 +1,61 @@
 package org.renhj.service.impl;
 
-import org.renhj.common.PageObject;
 import org.renhj.dao.SysUserDao;
+import org.renhj.dao.SysUserRoleDao;
 import org.renhj.entity.SysUser;
 import org.renhj.exception.ResourceNotExistException;
-import org.renhj.exception.ServiceException;
 import org.renhj.service.SysUserService;
+import org.renhj.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 @Service
 @Transactional(readOnly = true, propagation = Propagation.REQUIRED, timeout = 30)
 public class SysUserServiceImpl implements SysUserService {
     @Autowired
     private SysUserDao sysUserDao;
+    @Autowired
+    private SysUserRoleDao userRoleDao;
 
     @Override
-    public PageObject<SysUser> findUsersByUsernameWithPage(String username, Integer pageCurrent, Integer pageSize) {
-        int rows = sysUserDao.getRowCount(username); // 记录数
-        if (rows < 1){
-            throw new ServiceException("没有记录！");
+    public Map<?, ?> findUsersByUsernameWithPage(String username, Integer pageCurrent, Integer pageSize) {
+        try {
+            HashMap<String, Object> map = new HashMap<>();
+            int rowsCount = sysUserDao.getRowCount(username); // 查询总记录数
+            if (rowsCount<1) return map;// 如果没有记录 直接返回空map
+            // 查找用户记录
+            List<SysUser> users = sysUserDao.findUserWithPage((pageCurrent-1)*pageSize, pageSize, username);
+            ArrayList<Map<String, Object>> list = new ArrayList<>();
+            for (SysUser user: users){
+                Map<String, Object> userMap = JsonUtils.obj2Map(user);
+                Long[] roleIds = userRoleDao.findRoleIdsByUserId(user.getId());
+                userMap.put("roles", roleIds);
+                list.add(userMap);
+            }
+
+            map.put("row", rowsCount);
+            map.put("pageCurrent", pageCurrent);
+            map.put("pageSize", pageSize);
+            map.put("records",list);
+            map.put("pageCount", (rowsCount-1)/pageSize+1);
+            return map;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
         }
-        List<SysUser> users = sysUserDao.findUserWithPage((pageCurrent-1)*pageSize, pageSize, username);
-        PageObject<SysUser> pageObject = new PageObject<>();
-        pageObject.setRecords(users);
-        pageObject.setRow(rows);
-        pageObject.setPageSize(pageSize);
-        pageObject.setPageCount((rows - 1)/pageSize + 1);
-        pageObject.setPageCurrent(pageCurrent);
-        return pageObject;
     }
 
     @Override
     @Transactional(readOnly = false)
-    public SysUser saveUser(SysUser user) {
+    public SysUser saveUser(SysUser user, Long[] roleIds) {
         sysUserDao.saveUser(user);
+        userRoleDao.insertUserRole(user.getId(), roleIds);
         return user;
     }
 
